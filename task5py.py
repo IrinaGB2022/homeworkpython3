2f0d99f3426850c735c3cf48f60db8bea827c1d3
IrinaGB2022 <113421254+IrinaGB2022@users.noreply.github.com>
2022-12-07 01:17:53 +0300

    Delete task5py.py

--- task5py.py
+++
@@ -1,8 +0,0 @@
-ax = float(input('Kоординаты точки a по оси x:'))
-ay = float(input('Kоординаты точки a по оси y:'))
-bx = float(input('Kоординаты точки b по оси x:'))
-by = float(input('Kоординаты точки b по оси y:'))
-
-import math
-dist= math.sqrt((ax-bx)**2+(ay-by)**2)
-print(f'Растояние между точкой A до точки B = {dist}' )
